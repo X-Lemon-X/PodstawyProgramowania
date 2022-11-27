b06633c21ca175d5964321dd7adda890290c5ff9
pcWin <dodpat02@gmail.com>
2022-11-27 23:31:51 +0100

    dodano testowy plik do spradzani haszówecek XD

---
+++ a.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int agr, char *args[]){
+
+    if(agr >= 2){
+
+    }
+
+
+    int a=5, j=10;
+
+    float z,h =0.13;
+    z = h * j;
+    j =strlen("Wrocław");
+
+    int tabu[4] = {1,2,3,4};
+    int *ta;
+    ta = tabu;
+
+     for (size_t p = 0; p < 4; p++)
+        {
+            printf("tab[%i]: %i  ",p, *(ta+p));
+        }
+        printf("\n");
+
+    int tabli[3][4] = {1,2,3,4, 5,6,7,8, 9,10,11,12};
+    int (*tab)[4];
+
+    tab = tabli[0];
+
+    for (size_t i = 0; i < 3; i++)
+    {
+        for (size_t p = 0; p < 4; p++)
+        {
+            printf("tab[%i][%i]: %i  ",i,p, (*tab)[p]);
+        }
+        printf("\n");
+        tab++;
+    }
+    
+
+    int p = (65-strlen("Wrocław"))/2 ;
+    printf("->%f\n",z);
+int i=-12;
+if (0,0) i=1; else i=0;
+
+
+
+    return 0;
+}
