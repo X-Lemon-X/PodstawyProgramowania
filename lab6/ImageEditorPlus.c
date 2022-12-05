b59fa4716809dac34d3a898ace6f792645eef329
pat dud <patdud02@wp.pl>
2022-12-05 14:02:28 +0100

    zmiany z wykladu update

--- lab6/ImageEditorPlus.c
+++ lab6/ImageEditorPlus.c
@@ -8,10 +8,17 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image/stb_image_write.h"
 
-
+/*
+    funkcja ladujaca zdjęcie kożystając z funkcji biblioteki stbi_load wiecej informacji nie bedę przepisywał są dostępne w stb_image.h linia 138
+*/
 int LoadImage(Image *image ,char *path){
-    image->img = stbi_load("sky.jpg", &image->width, &image->height, &image->channels, 0);
-    if(image->img == NULL) return 1;
+
+    int x,y,n;
+    if(!stbi_info(path, &x, &y, &n)) return -2;
+
+
+    image->img = stbi_load(path, &image->width, &image->height, &image->channels, 0);
+    if(image->img == NULL) return -1;
     else return 0;
 }
 
