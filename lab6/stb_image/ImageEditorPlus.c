da80e07c856dc4367cee672a04a694017f9a1292
Kali <dodpat02@gmail.com>
2022-12-08 11:15:45 +0100

    update obrazy 2

--- lab6/stb_image/ImageEditorPlus.c
+++ lab6/stb_image/ImageEditorPlus.c
@@ -2,41 +2,66 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-#include "ImageEditorPlus.h"
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+#include "ImageEditorPlus.h"
+
 #define STBI_NO_STDIO
 
 #define DEFAULT_IMAGE_OUTPUT_FILE_NAME_PNG "out.png"
 #define DEFAULT_IMAGE_OUTPUT_FILE_NAME_JPG "out.jpg"
-#define PIXELMAXVALUE 255
+#define BUFFSIZE 256
 
+#define true 1
+#define false 0
+
+
+int SaveImage(Image *image);
+int SaveImage_as_png(Image image);
+int SaveImage_as_jpg(Image image);
+void Clear(Image *image);
+int CheckIfLoaded(Image image);
+void SetPixel(Image *image,size_t x, size_t y, Pixel pixel);
+void GetPixel(Image *image,size_t x, size_t y, Pixel *pixel);
+
+
+/*
 int main()
 {
     Image img;
-    char *pathIn = "/home/lemonx/IT/podstawyProgramowania/lab6/kostka.png";
-    char *pathOut = "/home/lemonx/IT/podstawyProgramowania/lab6/kostaout.png";
-    size_t sizeIN = 54;
+    char *pathIn = "/home/lemonx/IT/podstawyProgramowania/lab6/sampl.ppm";
+    char *pathOut = "/home/lemonx/IT/podstawyProgramowania/lab6/samplout.png";
+    size_t sizeIN = 53;
     size_t sizeOut = 56;
-    printf("--> %i",sizeIN);
-    int ret = LoadImage(&img, pathIn, sizeIN, pathOut, sizeOut);
-    printf("--> %i", ret);
+    printf("--> %i\n",sizeIN);
+    int ret = LoadImage(&img, pathIn, sizeIN, pathOut, sizeOut, IMAGE_TYPE_PNG);
+    printf("--> %i\n", ret);
+    
+    //GrayScaleAvarage(&img);
+    //Inverse(&img);
+    //EdgingPhoto(&img,100,100,100);
+    UseFullScale(&img);
     SaveImage_as_png(img);
     FreeMemory(&img);
 }
+*/
 
 /*
     sprawdza czy podana zotala ścieżka do pliku dla wyjścia i wejścia 
     funkcja ladujaca zdjęcie kożystając z funkcji biblioteki stbi_load 
     wiecej informacji nie bedę przepisywał są dostępne w stb_image.h linia 138
 */
-int LoadImage(Image *image ,char *inPath, size_t sizeIn ,char *outPath, size_t sizeOut){
+int LoadImage(Image *image ,char *inPath, size_t sizeIn ,char *outPath, size_t sizeOut, int saveAsImageType){
     
-    if(inPath == NULL) return -3;
+    Clear(image);
+
+    if(saveAsImageType!= IMAGE_TYPE_JPG && saveAsImageType!=IMAGE_TYPE_PNG) return ERROR_IMAGETYPE;
+
+    if(inPath == NULL) return NOINPUTFILE;
+
     if(outPath == NULL) image->outPath = NULL;
     else{
          image->outPath = (char*)calloc(sizeOut,sizeof(char));
@@ -48,22 +73,25 @@ int LoadImage(Image *image ,char *inPath, size_t sizeIn ,char *outPath, size_t s
     memcpy(image->inPath,inPath,sizeIn);
     image->sizeIn = sizeIn;
 
-
     int x,y,n;
-    if(!stbi_info(inPath, &x, &y, &n)) return -2;
+    if(!stbi_info(inPath, &x, &y, &n)) return FILEEXTENSIONNOTSUPPORTED;
     
-    image->img = stbi_load(inPath, &image->width, &image->height, &image->channels, 0);
-    if(image->img == NULL) return -1;
+    image->img = stbi_load(inPath, &image->width, &image->height, &image->channels, STBI_default);
+    if(image->img == NULL) return IMAGENOTLOADED;
     
-    image->imageSize = x * y * n;
+    image->imageSize = image->width * image->height * image->channels;
     image->loaded=1;
     return 0;
 }
 
-void CopyArray(char **dest, char *source, size_t sizeSource)
+int SaveImage(Image *image)
 {
-    *dest = (char*)calloc(sizeSource,sizeof(char));
-    memcpy(*dest,source, sizeSource);
+    if(!image->loaded) return IMAGENOTLOADED;
+
+    if(image->imageType == IMAGE_TYPE_JPG)
+        return SaveImage_as_jpg(*image);
+    else if(image->imageType == IMAGE_TYPE_PNG)
+        return SaveImage_as_png(*image);
 }
 
 /*
@@ -71,9 +99,8 @@ void CopyArray(char **dest, char *source, size_t sizeSource)
 */
 int SaveImage_as_png(Image image)
 {
-    char *path =image.outPath ;
+    char *path =image.outPath;
     if(path == NULL) path=DEFAULT_IMAGE_OUTPUT_FILE_NAME_PNG;
-
     return stbi_write_png(path, image.width, image.height, image.channels, image.img, image.width * image.channels);    
 }
 
@@ -106,50 +133,152 @@ void Clear(Image *image)
     image->channels=0;
     image->inPath=NULL;
     image->outPath=NULL;
-    image->in=NULL;
-    image->out=NULL;
 }
 
 int CheckIfLoaded(Image image)
 {
-    if(image.loaded) return 0;
-    else return 1;
+    return image.loaded;
 }
 
-int Inverse(Image *image)
+int GrayScale(Image *image)
 {
+    if(!image->loaded) return IMAGENOTLOADED;
+
+    Pixel pixel;
     for (size_t x = 0; x < image->width; x++)
     {
         for (size_t y = 0; y < image->height; y++)
         {
-            unsigned char red, green, blue;
-            GetPixel(image,x,y,red,green,blue);
-
-            red = PIXELMAXVALUE - red;
-            green = PIXELMAXVALUE - green;
-            blue = PIXELMAXVALUE - blue;
-
-            SetPixel(image,x,y,red,green,blue);
+            GetPixel(image,x,y,&pixel);
+            unsigned char valAvarage = (unsigned char)( pixel.red + pixel.green + pixel.blue )/3;
+            pixel.red = valAvarage;
+            pixel.green = valAvarage;
+            pixel.blue = valAvarage;
+            SetPixel(image,x,y,pixel);
         }
-        
     }
+    return OK;
+}
+
+int Inverse(Image *image)
+{
+    if(!image->loaded) return IMAGENOTLOADED;
+
+    Pixel pixel;
+    for (size_t x = 0; x < image->width; x++)
+    {
+        for (size_t y = 0; y < image->height; y++)
+        {
+            GetPixel(image,x,y,&pixel);
+            pixel.red = PIXELMAXVALUE - pixel.red;
+            pixel.green = PIXELMAXVALUE - pixel.green;
+            pixel.blue = PIXELMAXVALUE - pixel.blue;
+            SetPixel(image,x,y,pixel);
+        }
+    }
+    return OK;
+}
+
+int UseFullScale(Image *image)
+{
+    if(!(image->loaded)) return IMAGENOTLOADED;
+    Pixel min, max, pix;
+    FindMaxMinValues(image,&max,&min);
+
+    for (size_t x = 0; x < image->width; x++)
+    {
+        for (size_t y = 0; y < image->height; y++)
+        {
+            GetPixel(image,x,y,&pix);
+            if(max.red-min.red) pix.red = (pix.red - min.red)* max.red/ (max.red - min.red);
+            if(max.green-min.green) pix.green = (pix.green - min.green)* max.green/ (max.green - min.green);
+            if(max.blue-min.blue) pix.blue = (pix.blue - min.blue)* max.blue/ (max.blue - min.blue); 
+            SetPixel(image,x,y,pix);
+        }   
+    }
+
+    //printf("--max> R%i  G%i   B%i\n", max.red,max.green,max.blue);
+    //printf("--min> R%i  G%i   B%i\n", min.red,min.green,min.blue);
+
+    return OK;
+}
+
+int FindMaxMinValues(Image *image, Pixel *pixelMax, Pixel *pixelMin)
+{
+    if(!image->loaded) return IMAGENOTLOADED;
+
+    Pixel pixel;
+    GetPixel(image,0,0,&pixel);
+    CopyPixel(pixel,pixelMax);
+    CopyPixel(pixel,pixelMin);
+
+    for (size_t x = 0; x < image->width; x++)
+    {
+        for (size_t y = 0; y < image->height; y++)
+        {
+            GetPixel(image,x,y,&pixel);
+            if(pixel.red > pixelMax->red) pixelMax->red=pixel.red;
+            if(pixel.blue > pixelMax->blue) pixelMax->blue=pixel.blue;
+            if(pixel.green > pixelMax->green) pixelMax->green=pixel.green;
     
+            if(pixel.red < pixelMin->red) pixelMin->red=pixel.red;
+            if(pixel.green < pixelMin->green) pixelMin->green=pixel.green;
+            if(pixel.blue < pixelMin->blue) pixelMin->blue=pixel.blue;
+        }
+    }
+    return OK;
 }
 
-void SetPixel(Image *image,size_t x, size_t y, unsigned char red, unsigned char green, unsigned char blue)
+void CopyPixel(Pixel source, Pixel *destintion)
+{
+    destintion->red=source.red;
+    destintion->green=source.green;
+    destintion->blue=source.blue;
+}
+
+void ZeroPixel(Pixel *pixel)
+{
+    pixel->red=0;
+    pixel->green=0;
+    pixel->blue=0;
+}
+
+int EdgingPhoto(Image *image, unsigned char edgeRed,unsigned char edgeGreen, unsigned char edgeBlue )
+{   
+    if(!image->loaded) return IMAGENOTLOADED;
+
+    Pixel pixel;
+    for (size_t x = 0; x < image->width; x++)
+    {
+        for (size_t y = 0; y < image->height; y++)
+        {
+            GetPixel(image,x,y,&pixel);
+            if(pixel.red > edgeRed) pixel.red=PIXELMAXVALUE;
+            else pixel.red = PIXELMINVALUE;
+            if(pixel.green > edgeGreen) pixel.green=PIXELMAXVALUE;
+            else pixel.green= PIXELMINVALUE;
+            if(pixel.blue > edgeBlue) pixel.blue=PIXELMAXVALUE;
+            else pixel.blue = PIXELMINVALUE;
+            SetPixel(image,x,y,pixel);
+        }   
+    }
+    return OK;
+}
+
+void SetPixel(Image *image,size_t x, size_t y, Pixel pixel)
 {
     unsigned char *p = image->img;
     size_t index = x * image->channels + y * image->channels * image->width; 
-    *(p + index) = red;
-    *(p + index + 1) = green;
-    *(p + index + 2) = blue;
+    *(p + index) = pixel.red;
+    *(p + index + 1) = pixel.green;
+    *(p + index + 2) = pixel.blue;
 }
 
-void GetPixel(Image *image,size_t x, size_t y, unsigned char *red, unsigned char *green, unsigned char *blue)
+void GetPixel(Image *image,size_t x, size_t y, Pixel *pixel)
 {
     unsigned char *p = image->img;
     size_t index = x * image->channels + y * image->channels * image->width; 
-    *red = (uint8_t)*(p + index);
-    *green = (uint8_t)*(p + index + 1);
-    *blue = (uint8_t)*(p + index + 2);
+    pixel->red = (uint8_t)*(p + index);
+    pixel->green = (uint8_t)*(p + index + 1);
+    pixel->blue = (uint8_t)*(p + index + 2);
 }
--- lab6/stb_image/ImageEditorPlus.h
+++ lab6/stb_image/ImageEditorPlus.h
@@ -1,26 +1,41 @@
+
+
 #ifndef IMAGEEDITORPLUS_H
 #define IMAGEEDITORPLUS_H
 
+#define PIXELMAXVALUE 255
+#define PIXELMINVALUE 0
 
-#ifndef STBI_NO_STDIO
-#include <stdio.h>
-#endif // STBI_NO_STDIO
+#define IMAGENOTLOADED -1
+#define FILEEXTENSIONNOTSUPPORTED -2
+#define NOINPUTFILE -3
+#define ERROR_IMAGETYPE -7
+
+#define OK 0
+
+#define IMAGE_TYPE_JPG 1
+#define IMAGE_TYPE_PNG 2
 
 
 typedef struct {
     unsigned char *img;
-    int width, height, channels, loaded;
+    int width, height, channels, loaded, imageType;
     size_t sizeIn, sizeOut, imageSize;
     char *inPath, *outPath;
-    FILE *in;
-    FILE *out;
-
 } Image;
 
-int LoadImage(Image *image ,char *inPath, size_t sizeIn ,char *outPath, size_t sizeOut);
-int SaveImage_as_png(Image image);
-int SaveImage_as_jpg(Image image);
-void FreeMemory(Image *image);
+typedef struct {
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
+} Pixel;
 
+int LoadImage(Image *image ,char *inPath, size_t sizeIn ,char *outPath, size_t sizeOut, int saveAsImageType);
+int SaveImage(Image *image);
+int Inverse(Image *image);
+int UseFullScale(Image * image);
+void FreeMemory(Image *image);
+int EdgingPhoto(Image *image, unsigned char edgeRed,unsigned char edgeGreen, unsigned char edgeBlue);
+int GrayScale(Image *image);
 
 #endif
